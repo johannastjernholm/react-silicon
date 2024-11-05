8186959d10ca5e23e53e3b994963dd2489a95f96
Johanna Stjernholm <jo.stjernholm@gmail.com>
2024-11-05 10:39:01 +0100

    Create Brand component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./assets/css/main.css";
 import Header from "./components/header";
 import Hero from "./components/Hero";
+import Brands from "./components/Brands";
 function App() {
   return (
     <>
       <Header />
-      <Hero /> ;
+      <Hero />
+      <Brands />;
     </>
   );
 }
---
+++ src/components/Brands.jsx
@@ -0,0 +1,30 @@
+const Brands = () => {
+  return (
+    <>
+      <section id="brands">
+        <div className="container">
+          <div className="brand-box" id="brand-1">
+            <img src="src/assets/images/brand-1.svg" alt="" />
+          </div>
+          <div className="brand-box" id="brand-2">
+            <img src="src/assets/images/brand-2.svg" alt="" />
+          </div>
+          <div className="brand-box" id="brand-3">
+            <img src="src/assets/images/brand-3.svg" alt="" />
+          </div>
+          <div className="brand-box" id="brand-4">
+            <img src="src/assets/images/brand-4.svg" alt="" />
+          </div>
+          <div className="brand-box" id="brand-5">
+            <img src="src/assets/images/brand-5.svg" alt="" />
+          </div>
+          <div className="brand-box" id="brand-6">
+            <img src="src/assets/images/brand-6.svg" alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Brands;
