31a1bd73cb35033a3319b69b09f102577815771a
Johanna Stjernholm <jo.stjernholm@gmail.com>
2024-11-13 15:58:28 +0100

    Save dark mode setting in localstorage

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,11 +1,16 @@
 import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
+// TODO: Spara dark mode till localStorage
 
 const DarkThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("isDarkMode");
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
 
   useEffect(() => {
+    localStorage.setItem("isDarkMode", JSON.stringify(isDarkMode));
     document.body.setAttribute("data-theme", isDarkMode ? "dark" : "");
   }, [isDarkMode]);
 
