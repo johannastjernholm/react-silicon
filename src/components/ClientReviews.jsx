44346999668fad8fad01a84eda94716507587493
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-09-04 10:28:26 +0200

    Fix: ClientReviews not showing since API not working

--- src/components/ClientReviews.jsx
+++ src/components/ClientReviews.jsx
@@ -1,35 +1,42 @@
-import { useEffect, useState } from "react";
-import Review from "./Review";
-
 const ClientReviews = () => {
-  const [reviews, setReviews] = useState([]);
-
-  const getTestimonials = async () => {
-    try {
-      const res = await fetch("https://win24-assignment.azurewebsites.net/api/testimonials");
-      const data = await res.json();
-      setReviews(data);
-      console.log(data);
-    } catch (error) {
-      console.error("Failed to fetch", error);
-    }
-  };
-
-  useEffect(() => {
-    getTestimonials();
-  }, []);
-
   return (
     <>
       <section className="client-reviews">
         <div className="left">
           <h2>Clients are Loving Our App</h2>
         </div>
+        <div className="middle">
+          <img className="square-quote" src="src/assets/images/quotes.svg" alt="" />
+          <img className="img-stars" src="src/assets/images/rating1.svg" alt="" />
+          <p className="review-content">
+            I’ve used this banking service several times to send money and I’m truly impressed. Transfers are fast, and I can track everything
+            directly in the app. It feels safe and simple, even when I send larger amounts abroad. Customer support is quick to respond and always
+            helpful whenever I have questions.
+          </p>
+          <div className="user-container">
+            <img src="src/assets/images/fannie.svg" alt="" />
+            <div className="user-details">
+              <p className="user-name">Fannie Summers</p>
+              <p className="user-role">Designer</p>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <img className="square-quote" src="src/assets/images/quotes.svg" alt="" />
 
-        <div className="review-container">
-          {reviews.map((review, index) => (
-            <Review key={index} item={review} />
-          ))}
+          <img className="img-stars" src="src/assets/images/rating2.svg" alt="" />
+          <p className="review-content">
+            The best part about this service is how smooth the process is. Fees are clear and transparent, so I never have to worry about hidden
+            costs. The app is user-friendly, and transfers are completed within minutes. I highly recommend this bank to anyone who wants a secure way
+            to send money.
+          </p>
+          <div className="user-container">
+            <img src="src/assets/images/albert.svg" alt="" />
+            <div className="user-details">
+              <p className="user-name">Albert Flores</p>
+              <p className="user-role">Developer</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
